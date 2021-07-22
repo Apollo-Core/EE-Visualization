55e4ed530e2b840897dc0cff904674b08d0b3957
Fedor <fedor@dps.uibk.ac.at>
2021-07-22 11:04:00 +0200

    switch to JUnit 5

--- src/test/java/at/uibk/dps/ee/visualization/utils/UtilsVizGraphTest.java
+++ src/test/java/at/uibk/dps/ee/visualization/utils/UtilsVizGraphTest.java
@@ -1,8 +1,7 @@
 package at.uibk.dps.ee.visualization.utils;
 
-import static org.junit.Assert.*;
-import org.junit.Test;
-
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 import net.sf.opendse.model.Communication;
 import net.sf.opendse.model.Dependency;
 import net.sf.opendse.model.Element;
@@ -10,28 +9,28 @@ import net.sf.opendse.model.Task;
 
 public class UtilsVizGraphTest {
 
-	@Test
-	public void testElementCopyOkay() {
-		
-		Task task = new Task("t");
-		task.setAttribute("attrT", 1);
-		
-		Communication comm = new Communication("c");
-		comm.setAttribute("commT", "bla");
-		
-		Dependency dep = new Dependency("dep");
-		dep.setAttribute("attrD", 3.2);
-		
-		Element result1 = UtilsVizGraph.copy(task);
-		Element result2 = UtilsVizGraph.copy(comm);
-		Element result3 = UtilsVizGraph.copy(dep);
-		
-		assertTrue(result1 instanceof Task);
-		assertTrue(result2 instanceof Communication);
-		assertTrue(result3 instanceof Dependency);
-		
-		assertEquals(1, (int) result1.getAttribute("attrT"));
-		assertEquals("bla", result2.getAttribute("commT"));
-		assertEquals(3.2, result3.getAttribute("attrD"), 0.0);
-	}
+  @Test
+  public void testElementCopyOkay() {
+
+    Task task = new Task("t");
+    task.setAttribute("attrT", 1);
+
+    Communication comm = new Communication("c");
+    comm.setAttribute("commT", "bla");
+
+    Dependency dep = new Dependency("dep");
+    dep.setAttribute("attrD", 3.2);
+
+    Element result1 = UtilsVizGraph.copy(task);
+    Element result2 = UtilsVizGraph.copy(comm);
+    Element result3 = UtilsVizGraph.copy(dep);
+
+    assertTrue(result1 instanceof Task);
+    assertTrue(result2 instanceof Communication);
+    assertTrue(result3 instanceof Dependency);
+
+    assertEquals(1, (int) result1.getAttribute("attrT"));
+    assertEquals("bla", result2.getAttribute("commT"));
+    assertEquals(3.2, result3.getAttribute("attrD"), 0.0);
+  }
 }
